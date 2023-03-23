814c85c2aeab768740d34712bc148e9443802c8b
GusAkira <117558991+GusAkira@users.noreply.github.com>
2023-03-23 20:55:27 -0300

    1

---
+++ src/pages/Home/Componentes/EnviarGuia1/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react"; /*Modal */
+import "./styles.css";
+interface Open {
+  isOpenEnviar: boolean;
+  setIsOpenEnviar: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function EnviarGuia1({ isOpenEnviar, setIsOpenEnviar }: Open) {
+  return (
+    <div style={{ display: isOpenEnviar ? "flex" : "none" }}>
+      <h1>
+        fffffffffffffffffgfgggggggggggggffffffffffffffffffffffffffffffffffffffffffff
+      </h1>
+    </div> /*Modal*/
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,123 +1,29 @@
+import { useState } from "react"; /* Modal*/
+import { CadastroCliente1 } from "./Componentes/CadastroCliente1";
+import { EnviarGuia1 } from "./Componentes/EnviarGuia1";
 import "./styles.css";
+import logoTransparente from "../../img/COA linha/COA/favicon_1000x1000 recortada.png";
 
 export function Home() {
+  const [isOpen, setIsOpen] = useState(false); /*Modal*/
+  const [isOpenEnviar, setIsOpenEnviar] = useState(false); /*Modal*/
   return (
-    <body>
-      {/*  COMEÇO Barra de navegação */}
-      <header className="header">
-        <img src="/img/logo sem fundo.png" alt="logo" className="logo" />
-        <input className="busca" type="source" placeholder="busca"></input>
-        <nav className="nav">
+    <>
+      <body>
+        <CadastroCliente1 isOpen={isOpen} setIsOpen={setIsOpen} />
+        <EnviarGuia1
+          isOpenEnviar={isOpenEnviar}
+          setIsOpenEnviar={setIsOpenEnviar}
+        />
+        <header>
           <div>
-            <ul className="menu" />
-            <li>
-              <a href="#">Configs</a>
-            </li>
-            <li>
-              <a href="#">opçoes</a>
-            </li>
-            {/*  precisa colocar um icone de user */}
-            <li>
-              <a href="#"> USER</a>
-              <ul>
-                <li>
-                  <a href="#">OPÇOES</a>
-                </li>
-                <li>
-                  <a href="#">MEU PERFIL</a>
-                </li>
-                <li>
-                  <a href="#">DESLOGAR</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">notificaçao </a>
-            </li>
+            <img src={logoTransparente} />
+            <input type="source" placeholder="busca"></input>
           </div>
-        </nav>
-        {/*  FIM Barra de navegação  */}
-      </header>
-      {/*  COMEÇO Barra Lateral  */}
-      <aside className="aside">
-        <div className="blocousuario">
-          <img src="" alt="" className="foto" />
-          <h1 className="nomeusuario"> Usuario</h1>
-          <h1 className="cargo"> Adiministrador</h1>
-        </div>
-        <div className="navlateral">
-          <div className="menulateral">
-            <a href="signup.html" className="menulateral">
-              Cadastrar Colaborador
-            </a>
-            <a href="Cad.cliente.html" className="menulateral">
-              Cadastrar cliente
-            </a>
-            <a href="ChangePass.html" className="menulateral">
-              Alterar senha
-            </a>
-            <a href="UploadImg.html" className="menulateral">
-              Enviar guias
-            </a>
-          </div>
-        </div>
-      </aside>
-      {/*  FIM Barra Lateral  */}
-      {/*  Dashbord article */}
-      <article className="dashbord">
-        {/*  Caixinhas financeiras  */}
-        <section className="financeiras">
-          <div className="boxfinanceiras"></div>
-          <div className="boxfinanceiras"></div>
-          <div className="boxfinanceiras"></div>
-          <div className="boxfinanceiras"></div>
-        </section>
-        {/*  FIM Caixinhas financeiras  */}
-        <section className="notasfiscais">
-          {/* <Br></Br> */}
-          <div className="validaçao">
-            <div className="tabela">
-              <table className="table">
-                <tr className="titulotabela">
-                  <td className="tdi">Marcaçao</td>
-                  <td className="tdi">Codigo</td>
-                  <td className="tdi">Cliente</td>
-                  <td className="tdi">Valor</td>
-                  <td className="tdi">Vencimento</td>
-                  <td className="tdi">mes</td>
-                  <td className="tdi">Status</td>
-                  <td className="tdi">detalhes</td>
-                </tr>
-                <tr>
-                  <td> 1 </td>
-                  <td>1 </td>
-                  <td>1 </td>
-                  <td>1 </td>
-                  <td>1 </td>
-                  <td> 1</td>
-                  <td>1 </td>
-                  <td>1 </td>
-                </tr>
-                <tr>
-                  <td> 1</td>
-                  <td> 1</td>
-                  <td> 1</td>
-                  <td>1 </td>
-                  <td> 1</td>
-                  <td>1 </td>
-                  <td>1 </td>
-                  <td>1 </td>
-                </tr>
-              </table>
-            </div>
-          </div>
-        </section>
-        <section className="tarefas">
-          <div className="boxtarefas"></div>
-          <div className="boxtarefas"></div>
-          <div className="boxtarefas"></div>
-        </section>
-      </article>
-    </body>
+        </header>
+
+        <button onClick={() => setIsOpen(true)}>oi</button>
+      </body>
+    </>
   );
 }
