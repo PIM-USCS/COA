f9bf3c3af383acde538b16a86ff7891a5df5984d
Lucas <silvalucas1912@gmail.com>
2023-03-27 09:04:08 -0300

    reestruturacao da pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,6 @@ import {
   FileText,
   CaretDown,
   CaretUp,
-  ChartLine,
   ChartLineUp,
   ChartBar,
   File,
@@ -34,8 +33,8 @@ export function Home() {
   }
   return (
     <>
-      <main>
-        <header>
+      <main className="container-grid-home">
+        <header className="div-header-home">
           <div className="div-header">
             <div className="div-header-esquerda">
               <button className="botao-home">
@@ -51,211 +50,103 @@ export function Home() {
             </div>
           </div>
         </header>
-        <body>
-          <section className="barra-lateral">
-            <div className="usuario-barra-lateral">
-              <img src={GuiBalbino} alt="Foto usuario" />
+        <section className="div-sidenav-home">
+          <div className="usuario-barra-lateral">
+            <img src={GuiBalbino} alt="Foto usuario" />
+            <div>
+              <p>Guilherme Balbino</p>
+              <span>Administrador</span>
+            </div>
+          </div>
+          <div className="div-botoes">
+            <button className="botao-dashboard">
+              <Gauge size={32} weight="fill" />
+              Dashboard
+            </button>
+            <button className="botao-pages" onClick={HabilitarSubPaginas}>
+              <FileText size={32} weight="fill" />
+              Pages
+              <CaretDown
+                size={16}
+                weight="bold"
+                style={{
+                  display: isOpenPages ? "none" : "flex",
+                  marginLeft: "64px",
+                }}
+              />
+              <CaretUp
+                size={16}
+                weight="bold"
+                style={{
+                  display: isOpenPages ? "flex" : "none",
+                  marginLeft: "64px",
+                }}
+              />
+            </button>
+
+            <button
+              className="botao-secundario"
+              style={{ display: isOpenPages ? "flex" : "none" }}>
+              Deslogar
+            </button>
+            <button
+              className="botao-secundario"
+              style={{ display: isOpenPages ? "flex" : "none" }}>
+              Cadastrar colaborador
+            </button>
+            <button
+              className="botao-secundario"
+              style={{ display: isOpenPages ? "flex" : "none" }}>
+              Cadastrar cliente
+            </button>
+            <button
+              className="botao-secundario"
+              style={{ display: isOpenPages ? "flex" : "none" }}>
+              Alterar senha
+            </button>
+            <button
+              className="botao-secundario"
+              style={{ display: isOpenPages ? "flex" : "none" }}>
+              Enviar guias
+            </button>
+            <button
+              className="botao-secundario"
+              style={{ display: isOpenPages ? "flex" : "none" }}>
+              Finanças
+            </button>
+          </div>
+        </section>
+        <body className="div-content-home">
+          <div className="div-guias">
+            <div className="guias">
+              <ChartLineUp size={48} weight="bold" />
               <div>
-                <p>Guilherme Balbino</p>
-                <span>Administrador</span>
+                <h3>Guias próximo mês</h3>
+                <span>01</span>
               </div>
             </div>
-            <div className="div-botoes">
-              <button className="botao-dashboard">
-                <Gauge size={32} weight="fill" />
-                Dashboard
-              </button>
-              <button className="botao-pages" onClick={HabilitarSubPaginas}>
-                <FileText size={32} weight="fill" />
-                Pages
-                <CaretDown
-                  size={16}
-                  weight="bold"
-                  style={{
-                    display: isOpenPages ? "none" : "flex",
-                    marginLeft: "64px",
-                  }}
-                />
-                <CaretUp
-                  size={16}
-                  weight="bold"
-                  style={{
-                    display: isOpenPages ? "flex" : "none",
-                    marginLeft: "64px",
-                  }}
-                />
-              </button>
-
-              <button
-                className="botao-secundario"
-                style={{ display: isOpenPages ? "flex" : "none" }}
-              >
-                Deslogar
-              </button>
-              <button
-                className="botao-secundario"
-                style={{ display: isOpenPages ? "flex" : "none" }}
-              >
-                Cadastrar colaborador
-              </button>
-              <button
-                className="botao-secundario"
-                style={{ display: isOpenPages ? "flex" : "none" }}
-              >
-                Cadastrar cliente
-              </button>
-              <button
-                className="botao-secundario"
-                style={{ display: isOpenPages ? "flex" : "none" }}
-              >
-                Alterar senha
-              </button>
-              <button
-                className="botao-secundario"
-                style={{ display: isOpenPages ? "flex" : "none" }}
-              >
-                Enviar guias
-              </button>
-              <button
-                className="botao-secundario"
-                style={{ display: isOpenPages ? "flex" : "none" }}
-              >
-                Finanças
-              </button>
+            <div className="guias">
+              <ChartBar size={48} weight="fill" />
+              <div>
+                <h3>Guias em aberto</h3>
+                <span>01</span>
+              </div>
             </div>
-          </section>
-
-          <div className="container-menu">
-            <div className="div-guias">
-              <div className="guias">
-                <ChartLineUp size={48} weight="bold" />
-                <div>
-                  <h3>Guias próximo mês</h3>
-                  <span>01</span>
-                </div>
-              </div>
-              <div className="guias">
-                <ChartBar size={48} weight="fill" />
-                <div>
-                  <h3>Guias em aberto</h3>
-                  <span>01</span>
-                </div>
-              </div>
-              <div className="guias">
-                <File size={48} weight="fill" />
-                <div>
-                  <h3>Guias em análise</h3>
-                  <span>01</span>
-                </div>
-              </div>
-
-              <div className="guias">
-                <FolderOpen size={48} weight="fill" />
-                <div>
-                  <h3>Guias retornadas</h3>
-                  <span>01</span>
-                </div>
+            <div className="guias">
+              <File size={48} weight="fill" />
+              <div>
+                <h3>Guias em análise</h3>
+                <span>01</span>
               </div>
             </div>
 
-            <table className="tabela">
-              <thead>
-                <tr className="head-tabela">
-                  <th scope="col">
-                    <input className="" type="checkbox" />
-                  </th>
-                  <th scope="col">Código</th>
-                  <th scope="col">Cliente</th>
-                  <th scope="col">Valor</th>
-                  <th scope="col">Vencimento</th>
-                  <th scope="col">Mês Referente</th>
-                  <th scope="col">Estatus</th>
-                  <th scope="col">Detalhes</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <input className="" type="checkbox" />
-                  </td>
-                  <td>INV-0123</td>
-                  <td>Silas Roberto LTDA</td>
-                  <td>$123</td>
-                  <td>01 Jan 2045</td>
-                  <td>Nov 2022</td>
-                  <td>Regularizada</td>
-                  <td>
-                    <a className="" href="">
-                      Detail
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <input className="" type="checkbox" />
-                  </td>
-                  <td>INV-0123</td>
-                  <td>Guilherme Balbino LTDA</td>
-                  <td>$123</td>
-                  <td>01 Jan 2045</td>
-                  <td>Nov 2022</td>
-                  <td>Regularizada</td>
-                  <td>
-                    <a className="" href="">
-                      Detail
-                    </a>
-                  </td>
-                </tr>
-                <td>
-                  <input className="" type="checkbox" />
-                </td>
-                <td>INV-0123</td>
-                <td>Glauber Balsani EIRELI</td>
-                <td>$123</td>
-                <td>01 Jan 2045</td>
-                <td>Nov 2022</td>
-                <td>Regularizada</td>
-                <td>
-                  <a className="" href="">
-                    Detail
-                  </a>
-                </td>
-                <tr />
-
-                <tr>
-                  <td>
-                    <input className="" type="checkbox" />
-                  </td>
-                  <td>INV-0123</td>
-                  <td>Gustavo Akira S.A</td>
-                  <td>$123</td>
-                  <td>01 Jan 2045</td>
-                  <td>Nov 2022</td>
-                  <td>Regularizada</td>
-                  <td>
-                    <a className="" href="">
-                      Detail
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <input className="" type="checkbox" />
-                  </td>
-                  <td>INV-0123</td>
-                  <td>Lucas Aparecido S.A</td>
-                  <td>$123</td>
-                  <td>01 Jan 2045</td>
-                  <td>Nov 2022</td>
-                  <td>Regularizada</td>
-                  <td>
-                    <a className="" href="">
-                      Detail
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div className="guias">
+              <FolderOpen size={48} weight="fill" />
+              <div>
+                <h3>Guias retornadas</h3>
+                <span>01</span>
+              </div>
+            </div>
           </div>
         </body>
       </main>
