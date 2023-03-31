3d27464dc4201001d69ca6b63ed4a752086ef6e3
Lucas <silvalucas1912@gmail.com>
2023-03-31 15:57:03 -0300

    alterado forma de enviar guia

--- src/pages/Home/Componentes/EnviarGuia/index.tsx
+++ src/pages/Home/Componentes/EnviarGuia/index.tsx
@@ -1,5 +1,5 @@
 import { UserCircle, XCircle } from "phosphor-react";
-import React, { useState } from "react"; /*Modal */
+import React, { useState } from "react";
 import Swal from "sweetalert2"; /* Biblioteca da modal de alerta */
 
 import { ClienteProps } from "../../../../@types/Client";
@@ -14,50 +14,25 @@ interface Open {
 export function EnviarGuia({ isOpenEnviar, setIsOpenEnviar }: Open) {
   const [cliente, setCliente] = useState<ClienteProps>({} as ClienteProps);
 
-  const [valorPessoa, setvalorPessoa] = useState("");
-
-  function handleSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    const value = event.target.value;
-    setvalorPessoa(value);
-    console.log(`Valor selecionado: ${value}`);
-  }
-
   async function ConsultaCliente() {
-    if (valorPessoa === "PJ") {
-      try {
-        const { data } = await api.getClientByCNPJ(cliente.cnpj);
-        setCliente((prevState) => {
-          return {
-            ...prevState,
-            cnpj: data.cnpj,
-            nome: data.nome,
-          };
-        });
-      } catch (error) {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "O CNPJ informado não está cadastrado",
-        });
-      }
-    }
-    if (valorPessoa === "PF") {
-      try {
-        const { data } = await api.getClientByCNPJ(cliente.cpf);
-        setCliente((prevState) => {
-          return {
-            ...prevState,
-            cpf: data.cpf,
-            nome: data.nome,
-          };
-        });
-      } catch (error) {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "O CPF informado não está cadastrado",
-        });
-      }
+    try {
+      const { data } = await api.getClientByID(cliente.id);
+      setCliente((prevState) => {
+        return {
+          ...prevState,
+          cpf: data.cpf,
+          cnpj: data.cnpj,
+          nome: data.nome,
+        };
+      });
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Não foi possivel localizar o cliente com o ID informado",
+        color: "gray",
+        background: "black",
+      });
     }
   }
 
@@ -79,15 +54,19 @@ export function EnviarGuia({ isOpenEnviar, setIsOpenEnviar }: Open) {
         </header>
         <div>
           <div className="div-body-enviar-guia">
-            <select
-              name="mySelect"
-              id="mySelect"
-              className="option-pessoa"
-              value={valorPessoa}
-              onChange={handleSelectChange}>
-              <option value="PF">Pessoa Física</option>
-              <option value="PJ">Pessoa Jurídica</option>
-            </select>
+            <input
+              type="text"
+              placeholder="ID"
+              className="input-id-cliente"
+              value={cliente.id}
+              name="id"
+              onChange={(e) =>
+                setCliente({
+                  ...cliente,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            />
             <input
               type="text"
               placeholder="CNPJ/CPF"
---
+++ src/pages/Home/Componentes/Guias/index.tsx
@@ -0,0 +1,13 @@
+export function Guias() {
+  return (
+    <div className="div-tabela-body">
+      <p className="codigo">INV-0123</p>
+      <p className="cliente">Silas Roberto LTDA</p>
+      <p className="valor">R$123,00</p>
+      <p className="vencimento">01 Jan 2045</p>
+      <p className="mes">Nov 2022</p>
+      <p className="status">Regularizada</p>
+      <button className="detalhes">Detalhes</button>
+    </div>
+  );
+}
